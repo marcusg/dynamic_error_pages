27049fe79cdebf3f79f655560557a70c3b867523
Marcus Geißler <marcus3006@gmail.com>
2013-10-02 00:21:00 +0200

    bump version

--- lib/dynamic_error_pages/engine.rb
+++ lib/dynamic_error_pages/engine.rb
@@ -2,6 +2,7 @@ module DynamicErrorPages
   class Engine < ::Rails::Engine
 
     initializer 'dynamic_error_pages.update_app' do |app|
+      engine_name :dynamic_error_pages
       app.config.exceptions_app = app.routes
     end
 
--- lib/dynamic_error_pages/version.rb
+++ lib/dynamic_error_pages/version.rb
@@ -1,3 +1,3 @@
 module DynamicErrorPages
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
