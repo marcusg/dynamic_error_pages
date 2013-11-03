04920b1481926f32546ca32e1d6c4ab164ac503e
Marcus Geißler <marcus3006@gmail.com>
2013-11-03 20:12:17 +0100

    small fixes

--- lib/dynamic_error_pages/routes.rb
+++ lib/dynamic_error_pages/routes.rb
@@ -1,12 +1,10 @@
 module ActionDispatch::Routing
   class Mapper
     def dynamic_error_pages(opts={})
-      Rails.application.routes.draw do
-        # rails 4 need the :via option
-        via = Rails::VERSION::MAJOR > 3 ? {:via => :all} : {}
-        controller = opts[:controller] || "dynamic_error_pages/errors"
-        match '/:status', {:to => "#{controller}#show", :constraints => { :status => /\d{3}/ }}.merge(via)
-      end
+      # rails 4 need the :via option
+      via = Rails::VERSION::MAJOR > 3 ? {:via => :all} : {}
+      controller = opts[:controller] || "dynamic_error_pages/errors"
+      match '/:status', {:to => "#{controller}#show", :constraints => { :status => /\d{3}/ }}.merge(via)
     end
   end
 end
