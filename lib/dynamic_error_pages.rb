ddba049cb7c7299244f1f28d6ceea7ea0ae0d490
Marcus Geißler <marcus3006@gmail.com>
2013-11-03 15:26:14 +0100

    moved logic for route building

--- lib/dynamic_error_pages.rb
+++ lib/dynamic_error_pages.rb
@@ -1,2 +1,3 @@
 require "dynamic_error_pages/version"
 require "dynamic_error_pages/engine"
+require "dynamic_error_pages/routes"
--- spec/dummy3.2/config/routes.rb
+++ spec/dummy3.2/config/routes.rb
@@ -1,4 +1,6 @@
 Dummy::Application.routes.draw do
   resources :tests
   get :custom_exception, :to => "tests#custom_exception"
+
+  dynamic_error_pages
 end
